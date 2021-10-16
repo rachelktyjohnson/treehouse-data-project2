094063d7a96d47a03c76a1e0d3dfb7da2434cd75
Rachel Johnson <rachelktyjohnson@gmail.com>
2021-10-17 10:33:26 +1100

    Create data cleaning function

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
 from constants import TEAMS, PLAYERS
 
+
+def clean_data(raw_players):
+    clean_players = []
+
+    for player in raw_players:
+        new_player = {
+            'name': player['name'],
+            'height': int(player['height'].split(' ')[0]),
+            'experience': True if player['experience'] == 'YES' else False,
+            'guardian': player['guardians'].split(' and ')
+        }
+        clean_players.append(new_player)
+    return clean_players
+
+
 if __name__ == '__main__':
-    print("This is main!")
+    players = clean_data(PLAYERS)
+
