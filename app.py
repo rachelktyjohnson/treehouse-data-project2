d7c6fb4dc60dbad41c8b2cf2849adfd1b904d787
Rachel Johnson <rachelktyjohnson@gmail.com>
2021-10-17 11:08:36 +1100

    Create team balancing function (even exp players)

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from constants import TEAMS, PLAYERS
-
+import pprint
 
 def clean_data(raw_players):
     clean_players = []
@@ -15,6 +15,31 @@ def clean_data(raw_players):
     return clean_players
 
 
+def balance_teams(cleaned_players, raw_teams):
+    # split players into experienced and inexperienced and then recombine
+    exp_players = []
+    inexp_players = []
+    for player in cleaned_players:
+        if player['experience']:
+            exp_players.append(player)
+        else:
+            inexp_players.append(player)
+    all_players = exp_players + inexp_players
+
+    # then sort evenly into teams
+    balanced_teams = {}
+    for team_name in raw_teams:
+        balanced_teams[team_name] = []
+
+    while len(all_players) > 0:
+        for team_name in raw_teams:
+            balanced_teams[team_name].append(all_players.pop(0))
+
+    return balanced_teams
+
+
 if __name__ == '__main__':
     players = clean_data(PLAYERS)
+    teams = balance_teams(players, TEAMS)
+
 
